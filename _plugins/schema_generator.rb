d050acacfbb63ff67d511b4d8c853ae79884d5ef
Adel Nabiullin <throughyourhead@gmail.com>
2018-03-19 18:54:53 +0300

    Fix schema generator: typo

--- _plugins/schema_generator.rb
+++ _plugins/schema_generator.rb
@@ -27,7 +27,7 @@ class SchemaGenerator
     if ['yaml', 'json'].include? next_data_input[:type]
       return next_data_input[:file_data]
     else
-      subdirs[:attribures].each do |subdir|
+      subdirs[:attributes].each do |subdir|
         data[subdir] ||= {}
         data[subdir] = prepare_base(data[subdir], previous_dirs + [subdir], debug)
         data[subdir] = deep_merge({debug: previous_dirs}, data[subdir]) if debug
@@ -87,13 +87,13 @@ class SchemaGenerator
   end
 
   def get_subdirs(previous_dirs)
-    dirs = { attribures: [], keys: [] }
+    dirs = { attributes: [], keys: [] }
     path = @start_path.join(previous_dirs.join('/'))
     all  = Dir.entries("#{path}").select do |entry|
       File.directory?(File.join("#{path}", entry)) and !(entry =='.' || entry == '..')
     end
     all.each do |dir_name|
-      dir_name.start_with?('_') ? dirs[:keys] << dir_name : dirs[:attribures] << dir_name
+      dir_name.start_with?('_') ? dirs[:keys] << dir_name : dirs[:attributes] << dir_name
     end
     dirs
   end
@@ -107,7 +107,7 @@ class SchemaGenerator
       elsif !h1_val.is_a?(Hash) || !h2_val.is_a?(Hash)
         h2_val
       else
-        deep_merge(h1_val, h2_val)
+        deep_merge(h1[key], h2[key])
       end
     end
   end
