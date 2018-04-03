5eac998a6b5a7d0410ee91075cfe1ea1a5d6d42e
Ramil Sitdikov <sitdikov.ramil@gmail.com>
2018-04-03 12:59:54 +0300

    Update generator.

--- _plugins/schema_generator_service.rb
+++ _plugins/schema_generator_service.rb
@@ -12,82 +12,131 @@ class SchemaGeneratorService
     @result_path = Pathname.new(params[:result_path])
     @need_links  = params[:need_links] || false
     @output_type = params[:output_type] || 'yaml'
+    @debug       = params[:debug] || false
 
     @future_dirs = params[:build_path].sub(params[:start_path], '').split('/').select{|dir| dir unless dir.empty?}
   end
 
   def make
-    @data = prepare_base({}, [])
-    generate_schemas(@data, [], @future_dirs)
+    @data = prepare_data({}, [])
+    generate_schemas(@data, [], @future_dirs.compact)
   end
 
   private
 
-  def prepare_base(data = {}, previous_dirs = [], debug = false)
-    subdirs = get_subdirs(previous_dirs)
+  def prepare_data(data = {}, previous_dirs = [])
+    dir_data = get_dir_data(previous_dirs)
+    data = deep_merge(data, dir_data)
 
-    next_data_input = file_type_exist(previous_dirs)
-    if ['yaml', 'json'].include? next_data_input[:type]
-      return next_data_input[:file_data]
-    else
-
-      subdirs[:attributes].each do |subdir|
-        data[subdir] ||= {}
-        data[subdir] = prepare_base(data[subdir], previous_dirs + [subdir], debug)
-        data[subdir] = deep_merge({debug: previous_dirs}, data[subdir]) if debug
-      end
-      return data
+    subdirs = get_attribute_subdirs(previous_dirs)
+    subdirs.each do |subdir|
+      data[subdir] ||= {}
+      data[subdir] = prepare_data(data[subdir], previous_dirs + [subdir])
+      data[subdir] = deep_merge({debug: previous_dirs}, data[subdir]) if @debug
     end
+
+    data
   end
 
-  def generate_schemas(data = {}, previous_dirs = [], future_dirs = [], name_postfix = '', debug = false)
+  def generate_schemas(data = {}, previous_dirs = [], future_dirs = [], name_postfix = '')
     next_subdir = future_dirs.shift
 
-    unless next_subdir.nil? || next_subdir.empty?
-      data ||= {}
-      if replace_folder?(previous_dirs + [next_subdir])
-        data = prepare_base(data, previous_dirs + [next_subdir], debug)
-      else
-        data = deep_merge(data, prepare_base(data, previous_dirs + [next_subdir], debug))
-      end
-      data = deep_merge({debug: previous_dirs}, data) if debug
-      name_postfix = update_name_postfix(previous_dirs, name_postfix)
-      generate_schemas(data, previous_dirs + [next_subdir], future_dirs, name_postfix, debug)
+    unless next_subdir.nil?
+      name_postfix = get_name_postfix(previous_dirs, name_postfix)
+      new_previous_dirs = previous_dirs + [next_subdir]
+
+      dir_data = prepare_data(data, new_previous_dirs)
+      data = replace_folder?(new_previous_dirs) ? dir_data : deep_merge(data, dir_data)
+      data = deep_merge({debug: previous_dirs}, data) if @debug
+
+      generate_schemas(data, previous_dirs + [next_subdir], future_dirs, name_postfix)
     else
-      subdirs = get_subdirs(previous_dirs)
+      build_data(data, strip_key_dir(previous_dirs) + name_postfix) if build_folder?(previous_dirs)
 
-      if build_folder?(previous_dirs)
-        file_name = previous_dirs.last.strip[1..-1] + name_postfix
-        build(data, file_name)
-      end
-
-      subdirs[:keys].each do |subdir|
+      subdirs = get_key_subdirs(previous_dirs)
+      subdirs.each do |subdir|
         data_copy = data.clone
-        data_copy = generate_schemas(data_copy, previous_dirs, [subdir], name_postfix, debug)
+        data_copy = generate_schemas(data_copy, previous_dirs, [subdir], name_postfix)
       end
     end
   end
 
-  def file_type_exist(previous_dirs)
-    yaml_file = @start_path.join(previous_dirs.join('/')).join('schema.yaml')
-    return { type: 'yaml', file_data: YAML.load_file(yaml_file) } if yaml_file.exist?
-
-    json_file = @start_path.join(previous_dirs.join('/')).join('schema.json')
-    return { type: 'json', file_data: JSON.parse(File.read(json_file)) } if json_file.exist?
-
-    return { type: 'folder' }
+  def get_attribute_subdirs(previous_dirs)(previous_dirs)
+    path = @start_path.join(previous_dirs.join('/'))
+    subdirs = []
+    path.children(false).select do |entry|
+      dir, base = entry.split
+      subdirs << base.to_s if entry.directory? && base.to_s.start_with?('_')
+    end
+    subdirs
   end
 
-  def update_name_postfix(previous_dirs, name_postfix)
+  def get_key_subdirs(previous_dirs)
+    path = @start_path.join(previous_dirs.join('/'))
+    subdirs = []
+    path.children.each do |entry|
+      dir, base = entry.split
+      subdirs << base.to_s if entry.directory? && base.to_s.start_with?('_')
+    end
+    subdirs
+  end
+
+  def strip_key_dir(previous_dirs)
+    previous_dirs.last.strip[1..-1]
+  end
+
+  def get_dir_data(previous_dirs)
+    current_dir = @start_path.join(previous_dirs.join('/'))
+    files_with_ext = filter_files(current_dir)
+    data = {}
+    files_with_ext.each{|file| data = deep_merge(data, get_file_data(file))}
+    data
+  end
+
+  def filter_files(dir)
+    file_extensions = ['.yaml', '.yml', '.json']
+    dir.children.map do |entry|
+      { file: entry, ext: entry.extname } if entry.file? && file_extensions.include?(entry.extname)
+    end.compact
+  end
+
+  def get_file_data(file_with_ext)
+    case file_with_ext[:ext]
+    when *['.yaml', '.yml']
+      YAML.load_file(file_with_ext[:file])
+    when '.json'
+      JSON.parse(File.read(file_with_ext[:file]))
+    else
+      {}
+    end
+  end
+
+  def build_data(data, file)
+    case @output_type
+    when *['yaml', 'yml']
+      result_file = @result_path.join("#{file}.yaml")
+      File.open(result_file,"w"){|f| f.write(data.to_yaml)}
+    when 'json'
+      result_file = @result_path.join("#{file}.json")
+      File.open(result_file,"w"){|f| f.write(JSON.pretty_generate(data))}
+    end
+    create_link(file, result_file) if @need_links
+  end
+
+  def get_name_postfix(previous_dirs, name_postfix)
     name_file = @start_path.join(previous_dirs.join('/')).join('.name')
     return name_postfix unless name_file.exist?
     name_postfix + previous_dirs.last
   end
 
+  def rename_folder?(previous_dirs)
+    name_file = @start_path.join(previous_dirs.join('/')).join('.name')
+    name_file.exist?
+  end
+
   def build_folder?(previous_dirs)
     build_file = @start_path.join(previous_dirs.join('/')).join('.build')
-    return false unless build_file.exist?
-    true
+    build_file.exist?
   end
 
   def replace_folder?(previous_dirs)
@@ -95,38 +144,10 @@ class SchemaGeneratorService
     replace_file.exist?
   end
 
-  def build(data, file_name)
-    if @output_type == 'yaml'
-      result_file = @result_path.join("#{file_name}.yaml")
-      File.open(result_file,"w") do |f|
-        f.write(data.to_yaml)
-      end
-    else
-      result_file = @result_path.join("#{file_name}.json")
-      File.open(result_file,"w") do |f|
-        f.write(JSON.pretty_generate(data))
-      end
-    end
-    create_link(file_name, result_file)
-  end
-
   def create_link(file_name, source_file)
-    return unless @need_links
     link_name = @result_path.join("#{file_name}")
     FileUtils.remove_file(link_name) if link_name.exist?
     `ln -s #{source_file} #{link_name}`
-  end
-
-  def get_subdirs(previous_dirs)
-    dirs = { attributes: [], keys: [] }
-    path = @start_path.join(previous_dirs.join('/'))
-    all  = Dir.entries(path).select do |entry|
-      File.directory?(File.join(path, entry)) and !(entry =='.' || entry == '..')
-    end
-    all.each do |dir_name|
-      dir_name.start_with?('_') ? dirs[:keys] << dir_name : dirs[:attributes] << dir_name
-    end
-    dirs
   end
 
   def deep_merge(o1, o2)
@@ -140,17 +161,4 @@ class SchemaGeneratorService
       o1.merge(o2){|k, v1, v2| deep_merge(v1, v2)} :
       o2
   end
-end
-
-if __FILE__ == $0
-  root_path = File.expand_path($0).split('/')[0...-2]
-  params = {
-    start_path:  ARGV[0] || (root_path + ['schema_partials']).join('/'),
-    build_path:  ARGV[1] || (root_path + ['schema_partials']).join('/'),
-    result_path: ARGV[2] || (root_path + ['schemas/generated_schemas']).join('/'),
-    need_links:  ARGV[3] || true,
-    output_type: ARGV[4] || 'json'
-  }
-
-  SchemaGeneratorService.new(params).make
 end
