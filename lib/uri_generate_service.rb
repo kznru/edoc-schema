23270caffdc68c0243cc36853f3eb87ac84dae73
Egor Pavlov <pavlov_egor007@mail.ru>
2018-07-19 19:11:22 +0300

    Create sort rake task to show schemas on page

---
+++ lib/uri_generate_service.rb
@@ -0,0 +1,70 @@
+class UriGenerateService
+  def initialize(params)
+    @start_path  = params[:start_path]
+    @result_path = params[:result_path]
+    @previous_dir = ''
+    @part_of_name = ''
+    @record_only = "schema.json"
+    @array_names = []
+  end
+
+  def make
+    generate_uri(@start_path, @part_of_name, @previous_dir)
+  end
+
+  def generate_uri(current_path, part_of_name, previous_dir)
+    unless File.exist?(current_path+"/"+".stop_uri_generate")
+      part_of_name+=current_path.split('/').last if File.exist?(current_path+"/"+".name") && !(part_of_name.include?(current_path.split('/').last))
+      all_next_dirs = Dir.each_child(current_path).to_a # массив со всеми папками, возможно нужна проверка, что элемент массива именно папка
+      all_next_dirs.delete_if { |dir| !File.directory?(current_path + "/" + dir) }
+      unless all_next_dirs.include?(previous_dir)
+        next_dir = all_next_dirs.first
+      else
+        next_dir = all_next_dirs[all_next_dirs.index(previous_dir)+1]
+      end
+      previous_dir = current_path.split('/').last # записываем папку в которой находимся
+      full_name = previous_dir[1..-1]+part_of_name+".json"
+      add_in_story(full_name) if File.exist?(current_path+"/"+".build") && !@array_names.include?(full_name)
+      if next_dir.nil?
+        part_of_name = part_of_name.chomp(previous_dir) if File.exist?(current_path+"/"+".name") #удаляем имя папки из полного названия файла
+        current_path = current_path[0...current_path.rindex('/')] # возвращаемся на папку выше
+      else
+        current_path+="/"+next_dir
+      end
+      generate_uri(current_path, part_of_name, previous_dir)
+    else
+      puts 'All generate!'
+      sleep 2
+      sort(@array_names)
+    end
+  end
+
+  def add_in_story(full_name)
+    if full_name.split('_').last == @record_only
+      @array_names << full_name
+    end
+  end
+
+  def sort(array_names)
+    array_names =  array_names.group_by{|n| n.split('_').first}.to_a
+    array_names.uniq!
+    make_record(array_names)
+  end
+
+  def make_record(array_names)
+    array_names.each{ |names|
+        names.each{ |n|
+          File.open('./sorted_generation_schemas.md', 'a') do |file|
+            if n.kind_of?(String)
+              file.puts("\n")
+              file.puts("#{n}\n")
+              file.puts("\n")
+            else
+              n.each{|nn| file.puts("[#{nn}](#{@result_path+"/"+nn.to_s})\n")}
+            end
+          end
+        }
+      }
+  end
+
+end
---
+++ tasks/sorted.rake
@@ -0,0 +1,75 @@
+root_path = File.expand_path('.')
+require_relative [root_path, '_plugins', 'uri_generator_service'].join('/')
+
+
+head = "---
+layout: page
+title: Сгенерированные схемы
+permalink: /generated_schemas/
+---"
+
+namespace :sorted do
+
+  namespace :uri do
+    task :refresh_file do
+      File.truncate('./sorted_generation_schemas.md', 0)
+      File.open('./sorted_generation_schemas.md', 'a') do |file|
+        file.puts(head)
+      end
+    end
+
+    task generate_all: [:generate_query, :generate_regisrty, :generate_usluga, :generate_usluga_request] do
+    end
+
+    task :generate_query do
+      File.open('./sorted_generation_schemas.md', 'a') do |file|
+        file.puts("Query")
+      end
+      params = {
+        start_path:  [root_path, 'schema_partials/_query/'].join('/'),
+        result_path: ['/schemas/generated_schemas'].join('/'),
+      }
+      UriGeneratorService.new(params).make
+
+      puts 'Done query'
+    end
+
+    task :generate_regisrty do
+      File.open('./sorted_generation_schemas.md', 'a') do |file|
+        file.puts("Registry")
+      end
+      params = {
+        start_path:  [root_path, 'schema_partials/_registry'].join('/'),
+        result_path: ['/schemas/generated_schemas'].join('/'),
+      }
+      UriGeneratorService.new(params).make
+      puts 'Done regisrty'
+    end
+
+    task :generate_usluga do
+      File.open('./sorted_generation_schemas.md', 'a') do |file|
+        file.puts("Usluga")
+      end
+      params = {
+        start_path:  [root_path, 'schema_partials/_usluga'].join('/'),
+        result_path: ['/schemas/generated_schemas'].join('/'),
+      }
+      UriGeneratorService.new(params).make
+      puts 'Done usluga'
+    end
+
+    task :generate_usluga_request do
+      File.open('./sorted_generation_schemas.md', 'a') do |file|
+        file.puts("Usluga request")
+      end
+      params = {
+        start_path:  [root_path, 'schema_partials/_usluga_request'].join('/'),
+        result_path: ['schemas/generated_schemas'].join('/'),
+      }
+      UriGeneratorService.new(params).make
+      puts 'Done usluga_request'
+    end
+  end
+
+
+end
