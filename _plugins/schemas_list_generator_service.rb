f60658bebdaf205d0e06d47d75b533b7e704f401
Ramil Sitdikov <sitdikov.ramil@gmail.com>
2019-04-01 12:41:39 +0300

    Update sorted schemas list

--- _plugins/schemas_list_generator_service.rb
+++ _plugins/schemas_list_generator_service.rb
@@ -11,11 +11,11 @@ class SchemasListGeneratorService
     file = File.open(@schemas_list_path, 'w+')
     schemas_structure = JSON.parse(File.read(@schemas_structure_path))
 
-    @generated_schemas_list = prepare_generated_schemas_list
+    @generated_schemas_list = prepare_generated_schemas_list - except_keys
     write_header(file)
     write_table_of_contents(file, schemas_structure)
     write_content(file, schemas_structure)
-    puts "Following schemas not present in uslugas table:\n"
+    puts "\nFollowing schemas not present in uslugas table:\n" unless @generated_schemas_list.empty?
     puts @generated_schemas_list.sort
   end
 
@@ -77,5 +77,13 @@ class SchemasListGeneratorService
   def prepare_title_for_passport(passport)
     version_number = passport.first.split('_').last.to_i || 0
     "[#{passport.last}, версия ##{version_number}](/schemas/generated_schemas/#{passport.first}_usluga_schema.json)\n\n"
+  end
+
+  def except_keys
+    @except_keys ||= %w(
+      termination_of_municipal_property_lease_agreement_legal_entity_2
+      termination_of_municipal_property_lease_agreement_natural_person_2
+      termination_of_municipal_property_lease_agreement_natural_person_with_agent_2
+    )
   end
 end
