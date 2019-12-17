ff6bc9602554724ef72ebaf360db3f6f0d734cc4
Ramil Sitdikov <sitdikov.ramil@gmail.com>
2019-12-17 18:57:56 +0300

    Use optional definition for not required field

--- lib/services/schema_validator_service.rb
+++ lib/services/schema_validator_service.rb
@@ -2,6 +2,8 @@ require 'json_schemer'
 require 'byebug'
 
 class SchemaValidatorService
+  SCANNED_DOCUMENT_REF = 'http://edoc-schema.kzn.ru/document/ru/uslugi/definitions/scanned_document'.freeze
+
   def self.call
     new.call
   end
@@ -48,25 +50,37 @@ class SchemaValidatorService
       }
     end
     check_required_validity(data, file)
+    check_required_for_scanned_document(data, file)
   end
 
   def temporary_except_files
     @temporary_except_files ||= [
-      "schemas/document/ru/uslugi/definitions/green_report.json",
-      "schemas/geo/coordinates.json",
-      "schemas/geo/feature.json",
-      "schemas/geo/geojson.json",
-      "schemas/geo/geometry.json",
-      "schemas/geo/linearRingCoordinates.json",
-      "schemas/geo/lineStringCoordinates.json",
-      "schemas/geo/polygonCoordinates.json",
-    ].map{|file| Pathname.new([root_path, file].join('/'))}
+      'schemas/document/ru/uslugi/definitions/green_report.json',
+      'schemas/geo/coordinates.json',
+      'schemas/geo/feature.json',
+      'schemas/geo/geojson.json',
+      'schemas/geo/geometry.json',
+      'schemas/geo/linearRingCoordinates.json',
+      'schemas/geo/lineStringCoordinates.json',
+      'schemas/geo/polygonCoordinates.json'
+    ].map { |file| Pathname.new([root_path, file].join('/')) }
   end
 
   def check_required_validity(data, file)
-    required = data["required"] || []
-    properties = data["properties"]&.keys || []
+    required = data['required'] || []
+    properties = data['properties']&.keys || []
     excess_required = required - properties
     puts "\nExcess required in #{file}: #{excess_required}" unless excess_required.empty?
+  end
+
+  def check_required_for_scanned_document(data, file)
+    required = data['required'] || []
+    properties = data['properties'] || []
+    should_require = properties.map do |key, value|
+      key if value['$ref'] == SCANNED_DOCUMENT_REF
+    end.compact
+    need_required = should_require - required
+
+    puts "\nNot required scanned_document in #{file}: #{need_required}" unless need_required.empty?
   end
 end
